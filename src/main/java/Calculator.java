659f9bddf2382d92303897b486eb29738cad4e6e
Константин <ferrum9696@mail.ru>
2020-06-18 13:07:22 +0300

    Initial commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,38 @@
+public class Calculator {
+
+    public int addition(int a, int b) {
+        int result = a + b;
+        return result;
+    }
+
+    public double addition(double a, double b) {
+        double result = a + b;
+        return result;
+    }
+
+    public int subtraction(int a, int b) {
+        int result = a - b;
+        return result;
+    }
+
+    public double subtraction(double a, double b) {
+        double result = a - b;
+        return result;
+    }
+
+    public int multiplication(int a, int b) {
+        int result = a * b;
+        return result;
+    }
+
+    public double multiplication(double a, double b) {
+        double result = a * b;
+        return result;
+    }
+
+    public double division(double a, double b) {
+        double result = a / b;
+        return result;
+    }
+
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,37 @@
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class CalculatorTest {
+
+    private int valueA;
+    private int valueB;
+    private int expected;
+
+    public CalculatorTest(int valueA, int valueB, int expected) {
+        this.valueA = valueA;
+        this.valueB = valueB;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters(name = "{index}:sumOf({0}+{1})={2}")
+    public static Iterable<Object[]> dataForTest() {
+        return Arrays.asList(new Object[][]{
+                {1, 1, 2},
+                {2, 6, 8},
+                {18, 2, 20},
+                {13, 15, 28},
+                {1, 5, 6}
+        });
+    }
+
+    @Test
+    public void paramTest() {
+        assertEquals(expected, new Calculator().addition(valueA,valueB));
+    }
+}
