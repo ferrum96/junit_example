84695d9092d58642af27fcdfe82536f5c5f80ccb
Salov Konstantin <ferrum9696@mail.ru>
2020-06-22 15:40:53 +0300

    add SuiteTest

---
+++ src/test/java/SuiteTestClass.java
@@ -0,0 +1,11 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by retinka on 13.01.2016.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({AdditionTest.class, DivisionTest.class, MultiplicationTest.class, SubtractionTest.class} )
+public class SuiteTestClass {
+
+}
